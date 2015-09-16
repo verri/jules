cd456713e2d5d765b0610e62fc2459147cd134fe
Filipe Verri <filipeverri@gmail.com>
2015-09-16 12:53:02 -0300

    No default column names anymore.

--- include/dataframe/dataframe.hpp
+++ include/dataframe/dataframe.hpp
@@ -22,11 +22,11 @@ template <typename Coercion> base_dataframe<Coercion>::base_dataframe(std::initi
     std::size_t i = 0;
     for (auto& column : columns_) {
         auto& colname = column.name();
-        if (colname.empty())
-            colname = std::to_string(i);
-        if (colindexes_.find(colname) != colindexes_.end())
-            throw std::runtime_error{"repeated column name"};
-        colindexes_[colname] = i++;
+        if (!colname.empty()) {
+            if (colindexes_.find(colname) != colindexes_.end())
+                throw std::runtime_error{"repeated column name"};
+            colindexes_[colname] = i++;
+        }
     }
 }
 
@@ -38,24 +38,22 @@ template <typename Coercion> base_dataframe<Coercion>& base_dataframe<Coercion>:
 template <typename Coercion>
 base_dataframe<Coercion>& base_dataframe<Coercion>::colbind(const column_t& column)
 {
-    auto tmp = column;
+    const auto& name = column.name();
 
-    auto& name = tmp.name();
-    if (name.empty())
-        name = std::to_string(columns_.size());
+    if (!name.empty()) {
+        auto it = colindexes_.find(name);
+        if (it != colindexes_.end())
+            throw std::runtime_error{"column already exists"};
+    }
 
-    auto it = colindexes_.find(name);
-    if (it != colindexes_.end())
-        throw std::runtime_error{"column already exists"};
-
-    if (!null() && nrow() != tmp.size())
+    if (!null() && nrow() != column.size())
         throw std::runtime_error{"invalid column size"};
 
     if (null())
-        nrow_ = tmp.size();
+        nrow_ = column.size();
 
-    colindexes_[name] = columns_.size();
-    columns_.push_back(std::move(tmp));
+    columns_.push_back(column);
+    if (!name.empty()) colindexes_[name] = columns_.size();
 
     return *this;
 }
@@ -68,12 +66,11 @@ template <typename Coercion> base_dataframe<Coercion>& base_dataframe<Coercion>:
 template <typename Coercion> base_dataframe<Coercion>& base_dataframe<Coercion>::colbind(column_t&& column)
 {
     auto& name = column.name();
-    if (name.empty())
-        name = std::to_string(columns_.size());
-
-    auto it = colindexes_.find(name);
-    if (it != colindexes_.end())
-        throw std::runtime_error{"column already exists"};
+    if (!name.empty()) {
+        auto it = colindexes_.find(name);
+        if (it != colindexes_.end())
+            throw std::runtime_error{"column already exists"};
+    }
 
     if (!null() && nrow() != column.size())
         throw std::runtime_error{"invalid column size"};
@@ -81,7 +78,7 @@ template <typename Coercion> base_dataframe<Coercion>& base_dataframe<Coercion>:
     if (null())
         nrow_ = column.size();
 
-    colindexes_[name] = columns_.size();
+    if (!name.empty()) colindexes_[name] = columns_.size();
     columns_.push_back(std::move(column));
 
     return *this;
