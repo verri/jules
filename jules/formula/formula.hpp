fa98e24951cc052c01e5068ad49e0c1ffab47dd0
Filipe Verri <filipeverri@gmail.com>
2017-01-14 20:21:41 -0700

    formula: Expected solution comments.

---
+++ jules/formula/formula.hpp
@@ -0,0 +1,7 @@
+// using namespace jules::formula_literals;
+// model:     "a"_v, "b"_v, "c"_v           var<numeric>("a"), var<numeric>("c"), var<numeric>("c")
+// formula:   ("a"_v, "b"_v, "c"_v)->("y")  (var<numeric>("a"), var<numeric>("c"), var<numeric>("c"))->(var<numeric>("y"))
+
+// using namespace jules::extended_formula_literals;
+// model:   "a"_n, "b"_n, "c"_n
+// formula: ("a"_n, "b"_n, "c"_n)->("y"_n)
