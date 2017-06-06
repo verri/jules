afa23a6499691c637cd3bfb6839e3be2d0752b1d
Filipe Verri <filipeverri@gmail.com>
2017-06-06 14:28:47 -0300

    random: Index sampling without reposition.

--- jules/base/random.hpp
+++ jules/base/random.hpp
@@ -15,12 +15,17 @@
 namespace jules
 {
 
-static inline auto construct_default_random_engine() noexcept
-{
-  return std::mt19937{std::random_device{}()};
-}
+struct no_replacement_t {
+  constexpr explicit no_replacement_t() = default;
+};
+
+static constexpr auto no_replacement = no_replacement_t{};
+
+static inline auto construct_default_random_engine() noexcept { return std::mt19937{std::random_device{}()}; }
 
 static thread_local auto random_engine = construct_default_random_engine();
+
+static inline auto rand() { return std::generate_canonical<numeric, numeric_traits<numeric>::digits>(random_engine); }
 
 template <typename Dist> auto sample(Dist& dist) { return dist(random_engine); }
 
@@ -42,10 +47,46 @@ static inline auto bernoulli_sample(numeric p)
   return sample(dist);
 }
 
+static inline auto uniform_index_sample(index_t n, index_t end)
+{
+  auto dist = std::uniform_int_distribution<index_t>(0u, end - 1);
+  return sample(n, dist);
+}
+
 static inline auto uniform_index_sample(index_t end)
 {
   auto dist = std::uniform_int_distribution<index_t>(0u, end - 1);
   return sample(dist);
+}
+
+static inline auto uniform_index_sample(index_t n, index_t end, no_replacement_t) -> const_vector<index_t>
+{
+  // inspired by John D. Cook, http://stackoverflow.com/a/311716/15485
+  auto t = index_t{0};
+  auto m = index_t{0};
+
+  auto result = std::vector<index_t>();
+  result.reserve(n);
+
+  while (m < n) {
+    const auto u = rand();
+
+    if ((end - n) * u < n - m) {
+      result.push_back(t);
+      ++m;
+    }
+    ++t;
+  }
+
+  return {std::move(result)};
+}
+
+template <typename Rng, typename U = range::range_value_t<Rng>, typename = meta::requires<range::Range<Rng>>>
+auto discrete_index_sample(index_t n, const Rng& rng)
+{
+  static_assert(std::is_convertible<U, double>::value, "weights must be convertible to double");
+  auto dist = std::discrete_distribution<index_t>(range::begin(rng), range::end(rng));
+  return sample(n, dist);
 }
 
 template <typename Rng, typename U = range::range_value_t<Rng>, typename = meta::requires<range::Range<Rng>>>
--- test/base/random.cpp
+++ test/base/random.cpp
@@ -1,5 +1,6 @@
 #include "jules/base/random.hpp"
 
+#include <algorithm>
 #include <catch.hpp>
 
 TEST_CASE("Basic random operations", "[random]")
@@ -9,6 +10,17 @@ TEST_CASE("Basic random operations", "[random]")
   CHECK(bernoulli_sample(0.0) == false);
   CHECK(bernoulli_sample(1.0) == true);
 
-  auto samples = bernoulli_sample(10u, 0.5);
+  const auto samples = bernoulli_sample(10u, 0.5);
   CHECK(samples.size() == 10u);
+
+  const auto ix = uniform_index_sample(10, 10, no_replacement);
+  CHECK(ix.size() == 10u);
+
+  {
+    auto answer = std::array<index_t, 10>{{}};
+    std::iota(begin(answer), end(answer), 0u);
+
+    const auto[ita, itb] = std::mismatch(ix.begin(), ix.end(), begin(answer));
+    CHECK(ita == ix.end());
+  }
 }
